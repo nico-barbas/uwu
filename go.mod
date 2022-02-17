45e67790cc41d7c9faed0ffb56c01e31d66f3d9a
nico-ec <64324649+nico-ec@users.noreply.github.com>
2022-02-18 00:10:03 +0100

    Added profiling

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/pkg/profile v1.6.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/mobile v0.0.0-20210902104108-5d9a33257ab5 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/nico-ec/uwu/editor"
+	"github.com/pkg/profile"
 )
 
 func main() {
+	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	ebiten.SetWindowSize(1600, 900)
 	ebiten.SetWindowDecorated(false)
 	ebiten.SetRunnableOnUnfocused(false)
@@ -16,6 +19,10 @@ func main() {
 	ed := editor.NewEditor()
 
 	if err := ebiten.RunGame(ed); err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
+	// fmt.Println("Serving pprof on port 8080")
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	panic(err)
+	// }
 }
